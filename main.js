9027bd57822a10a9cd6062b41ec6e97d55fdbac6
John Meneses <jmmenes90@gmail.com>
2021-04-06 14:16:11 -0400

    Still need to implement dark mode & alt colors.

--- main.js
+++ main.js
@@ -16,7 +16,9 @@
  * is the first argument to `repeat`, currently set at 10.
  */
 const gridWidth = 10;
+let isMouseDown = false;
 let count = 0;
+let color_selected = "color-2";
 while (count <= gridWidth * gridWidth) {
   const canvas = document.querySelector(".canvas");
   const div = document.createElement("div");
@@ -51,6 +53,15 @@ while (count <= gridWidth * gridWidth) {
 // Add queries for all your squares, palette colors, and brush here.
 // (Note the singular or plural used in that sentence!)
 
+// Get the current brush HTML element
+const brush = document.querySelector(".current-brush");
+
+// Get the palette color HTML elements
+const paletteColors = document.querySelectorAll(".palette-color");
+
+// Get all squares
+const squares = document.querySelectorAll(".square");
+
 /****************************
  * EVENT LISTENER FUNCTIONS *
  ****************************/
@@ -60,6 +71,53 @@ while (count <= gridWidth * gridWidth) {
 // empty at first, though a console.log just to know they're being
 // run as event listeners (after the next step is set up) isn't a
 // bad idea for testing purposes.
+
+// For each palette color
+for (const each of paletteColors) {
+  // When the palette color is clicked
+  each.addEventListener("click", function (event) {
+    const elementIsClicked = event.target;
+    // Get the classes of the brush in an array
+    paletteClicked(elementIsClicked.className.split(" ")[1]);
+  });
+}
+
+// For each square
+for (const each of squares) {
+  // If user clicks down
+  each.addEventListener("mousedown", (e) => {
+    isMouseDown = true;
+  });
+
+  // If user clicks up
+  each.addEventListener("mouseup", (e) => {
+    if (isMouseDown === true) {
+      isMouseDown = false;
+    }
+  });
+
+  // Add a 'mouseenter' event listener
+  // The mouseenter event is fired at an element when a mouse
+  // is initially moved so that its hot spot is within the
+  // element at which the event was fired.
+  each.addEventListener("mouseenter", (e) => {
+    // If user clicks down
+    if (isMouseDown === true) {
+      const elementIsClicked = e.target;
+      gridClicked(elementIsClicked);
+    }
+  });
+
+  function paletteClicked(color) {
+    color_selected = color;
+    brush.className = "current-brush " + color;
+  }
+
+  function gridClicked(element) {
+    // Set the square's class to 'square ' + color
+    element.className = "square " + color_selected;
+  }
+}
 
 /**************************
  * WIRING IT ALL TOGETHER *
